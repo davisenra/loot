1f256b33ece16dc36bbf5bcc5aff3b5bfccd288c
davisenra <davi.senra98@gmail.com>
2026-05-23 23:44:47 -0300

    feat(api): cascade on delete

---
+++ pocketbase/pb_migrations/1779590643_updated_tags.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1219621782")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": true,
+    "collectionId": "_pb_users_auth_",
+    "help": "",
+    "hidden": false,
+    "id": "relation2809058197",
+    "maxSelect": 0,
+    "minSelect": 0,
+    "name": "user",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1219621782")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": false,
+    "collectionId": "_pb_users_auth_",
+    "help": "",
+    "hidden": false,
+    "id": "relation2809058197",
+    "maxSelect": 0,
+    "minSelect": 0,
+    "name": "user",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
---
+++ pocketbase/pb_migrations/1779590660_updated_orders.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3527180448")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": true,
+    "collectionId": "_pb_users_auth_",
+    "help": "",
+    "hidden": false,
+    "id": "relation2375276105",
+    "maxSelect": 0,
+    "minSelect": 0,
+    "name": "user",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3527180448")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": false,
+    "collectionId": "_pb_users_auth_",
+    "help": "",
+    "hidden": false,
+    "id": "relation2375276105",
+    "maxSelect": 0,
+    "minSelect": 0,
+    "name": "user",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
